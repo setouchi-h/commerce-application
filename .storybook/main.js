eb92f25ca9df6f7d559034bc86044ac0ada9880a
setouchi-h <setouchi.kazuki@gmail.com>
2023-02-04 12:50:27 +0900

    環境設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,31 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+const path = require("path")
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-property-in-object",
+    ],
+  }),
+  webpackFianl: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ]
+
+    return config
+  },
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,29 +3,29 @@ import Head from "next/head"
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-html,
-body,
-textarea {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
 
-* {
-  box-sizing: border-box;
-}
+  * {
+    box-sizing: border-box;
+  }
 
-a {
-  cursor: pointer;
-  text-decoration: none;
-  transition: .25s;
-  color: #000;
-}
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
 
-ol, ul {
-  list-style: none;
-}
+  ol, ul {
+    list-style: none;
+  }
 `
 
 const MyApp = ({ Component, pageProps }: AppProps) => {

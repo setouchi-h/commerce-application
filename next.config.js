9b80f32e1b01c730bb5e45acf3846f638948f179
setouchi-h <setouchi.kazuki@gmail.com>
2023-02-04 14:18:35 +0900

    APIクライアント実装

--- next.config.js
+++ next.config.js
@@ -13,8 +13,16 @@ const nextConfig = {
       }
     }
 
-    return compilerConfig0000000000
+    return compilerConfig
   })(),
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
---
+++ src/services/users/get-user.ts
@@ -0,0 +1,35 @@
+import type { ApiContext, User } from "types"
+import { fetcher } from "utils"
+
+export type GetUserParams = {
+  id: number
+}
+
+/**
+ * ユーザAPI（個別取得）
+ * @param context APIコンテキスト
+ * @param params パラメータ
+ * @returns ユーザ
+ */
+const getUser = async (context: ApiContext, { id }: GetUserParams): Promise<User> => {
+  /**
+    // ユーザAPI
+    // サンプルレスポンス
+    {
+    "id": 1,
+    "username": "taketo",
+    "displayName": "Taketo Yoshida",
+    "email": "taketo@example.com",
+    "profileImageUrl": "/users/1.png",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    }
+     */
+  return await fetcher(`${context.apiRootUrl.replace(/\/$/g, "")}/users/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+}
+
+export default getUser
